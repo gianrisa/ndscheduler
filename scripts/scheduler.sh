62c5b223e78a83ed36e6bd914ec15b9d2278c0e2
Gianfranco Risaliti <gianfranco.risaliti@gmail.com>
2017-11-12 23:34:30 +0100

    Added the script to run ndscheduler as daemon

---
+++ scripts/scheduler.sh
@@ -0,0 +1,2 @@
+cd /home/lx_risalgia/stockmonitor/ndscheduler/
+. /home/lx_risalgia/stockmonitor/ndscheduler/.venv/bin/activate && NDSCHEDULER_SETTINGS_MODULE=simple_scheduler.settings PYTHONPATH=.: exec /home/lx_risalgia/stockmonitor/ndscheduler/.venv/bin/python simple_scheduler/scheduler.py &
